9a812c0d4e84abeebfea4a0fd42dbff7bf7e7e88
beknazar <beknazar23@gmail.com>
2016-10-20 15:17:48 +0900

    Update the format of the file

--- parsave.m
+++ parsave.m
@@ -1,9 +1,12 @@
 function parsave(varargin)
+    
+    for i = 1:numvars
+       eval([inputname(i+2),' = varargin{i};']);  
+    end
 
-for i = 1:numvars
-   eval([inputname(i+2),' = varargin{i};']);  
-end
-save('-mat', fname, inputname(3));
-for i = 2:numvars    
-    save('-mat', fname, inputname(i+2), '-append');
+    save('-mat', fname, inputname(3));
+
+    for i = 2:numvars    
+        save('-mat', fname, inputname(i+2), '-append');
+    end
 end
